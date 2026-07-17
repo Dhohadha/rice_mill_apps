f416e4052032256b1f160eabf47354a4369cd0cf
Dhohada <dohadanarayana@gmail.com>
2026-07-17 10:07:22 +0530

    separated admin and server

--- admin/android/app/src/main/kotlin/com/RiceMill_admin/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.RiceMill_admin.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- server/models/DeviceToken.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const deviceTokenSchema = new mongoose.Schema({
-  token: { type: String, required: true, unique: true },
-  userEmail: { type: String, required: true, index: true },
-  lastUpdated: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('DeviceToken', deviceTokenSchema);
