3eff95e73db182673320abd37518e86bdeab73f0
Dhohada <dohadanarayana@gmail.com>
2026-09-12 16:22:32 +0530

    Fix SharedPreferences import in fcm_service.dart and confirm successful release APK build

--- lib/services/fcm_service.dart
+++ lib/services/fcm_service.dart
@@ -4,6 +4,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'notification_service.dart';
 import 'api_service.dart';
 import 'alarm_service.dart';
